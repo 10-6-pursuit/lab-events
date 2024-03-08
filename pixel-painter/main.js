8bf4898dbbea1f8b3aef55e5800d91fd32ef52bc
Runquan (Ray) Zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-03-08 09:20:07 -0500

    change grid from 10x10 to 20x20

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -3,22 +3,30 @@ const currentColor = document.querySelector("#current-color")
 const colors = document.querySelectorAll(".color")
 
 for (let color of colors) {
-    const currColor = color.getAttribute("style")
+
+    const currColor = color.getAttribute("style").split(" ")[1]
+
     color.addEventListener("click", () => {
-    currentColor.style.background = currColor.split(" ")[1]
-    
+
+    currentColor.style.background = currColor
+
     })
 }
 
 const cells = document.querySelectorAll(".cell")
 
 for (let cell of cells) {
+
     cell.addEventListener("click", (event) =>{
+
     const currColor = event.target.parentNode.parentNode.querySelector("#current-color")
-    const color = currColor.getAttribute("style")
-    cell.style.background = color.split(" ")[1].slice(0, -1)
-    console.log(color.split(" ")[1].slice(0, -1))
+
+    const color = currColor.getAttribute("style").split(" ")[1].slice(0, -1)
+
+    cell.style.background = color
+
     })
 }
+
 
 
