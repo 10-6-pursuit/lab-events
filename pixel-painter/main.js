52619fcc3507541015f2065650e64940cfb4a68a
Runquan (Ray) Zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-03-08 01:47:07 -0500

    complete pixel painter

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -5,7 +5,20 @@ const colors = document.querySelectorAll(".color")
 for (let color of colors) {
     const currColor = color.getAttribute("style")
     color.addEventListener("click", () => {
-        currentColor.style.background = currColor.split(" ")[1]
+    currentColor.style.background = currColor.split(" ")[1]
+    
     })
 }
+
+const cells = document.querySelectorAll(".cell")
+
+for (let cell of cells) {
+    cell.addEventListener("click", (event) =>{
+    const currColor = event.target.parentNode.parentNode.querySelector("#current-color")
+    const color = currColor.getAttribute("style")
+    cell.style.background = color.split(" ")[1].slice(0, -1)
+    console.log(color.split(" ")[1].slice(0, -1))
+    })
+}
+
 
