ee7912d0d446823f912a2b7ed8a0e6595573b26c
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2024-03-16 11:32:09 -0400

    working on events lab

---
+++ tic-tac-toe/main.js
@@ -0,0 +1,97 @@
+
+let plays = new Array(9).fill("");
+const playsLimit = 9;
+let playsCount = 0;
+let previousPlayer = "X";
+let isThereWinner = false;
+
+
+const title = document.querySelector("h1");
+const square = document.querySelectorAll(".square");
+square.forEach((ele) => ele.addEventListener("click", handlePlay));
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", resetGame);
+function handlePlay(e) {
+  previousPlayer = previousPlayer === "X" ? "O" : "X";
+  e.target.innerText = previousPlayer;
+  e.target.style.pointerEvents = "none";
+  playsCount++;
+  setPlay(e);
+  checkForWin();
+}
+function setPlay(e) {
+  plays[e.target.id] = previousPlayer;
+}
+function checkForWin() {
+  if (
+    (plays[0] === plays[1] &&
+      plays[1] === plays[2] &&
+      plays[0] &&
+      plays[1] &&
+      plays[2]) ||
+    (plays[3] === plays[4] &&
+      plays[4] === plays[5] &&
+      plays[3] &&
+      plays[4] &&
+      plays[5]) ||
+    (plays[6] === plays[7] &&
+      plays[7] === plays[8] &&
+      plays[6] &&
+      plays[7] &&
+      plays[8]) ||
+    (plays[0] === plays[3] &&
+      plays[3] === plays[6] &&
+      plays[0] &&
+      plays[3] &&
+      plays[6]) ||
+    (plays[1] === plays[4] &&
+      plays[4] === plays[7] &&
+      plays[1] &&
+      plays[4] &&
+      plays[7]) ||
+    (plays[2] === plays[5] &&
+      plays[5] === plays[8] &&
+      plays[2] &&
+      plays[5] &&
+      plays[8]) ||
+    (plays[0] === plays[4] &&
+      plays[4] === plays[8] &&
+      plays[0] &&
+      plays[4] &&
+      plays[8]) ||
+    (plays[2] === plays[4] &&
+      plays[4] === plays[6] &&
+      plays[2] &&
+      plays[4] &&
+      plays[6])
+  ) {
+    isThereWinner = true;
+    showFinalResult(previousPlayer);
+  }
+  if (playsCount === playsLimit && !isThereWinner) {
+    showFinalResult();
+  }
+}
+function showFinalResult(winner = "") {
+  title.innerText = winner
+    ? `${winner} Wins!`
+    : "Draw";
+  square.forEach((ele) => (ele.style.pointerEvents = "none"));
+}
+function resetGame() {
+  playsCount = 0;
+  previousPlayer = "X";
+  isThereWinner = false;
+  title.innerText = "Tic Tac Toe";
+  plays = new Array(9).fill("");
+  square.forEach((ele) => {
+    ele.innerText = "";
+    ele.style.pointerEvents = "auto";
+  });
+}
+
+
+
+
+
+
