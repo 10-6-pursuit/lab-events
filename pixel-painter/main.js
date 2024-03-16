74f395f1aab6ce483104a7d266276dad3a135ced
JorgeLG87 <jorge85_6@hotmail.com>
2024-03-16 11:31:55 -0400

    Pixel-Painter completed

---
+++ pixel-painter/main.js
@@ -0,0 +1,34 @@
+const main = document.querySelector("#canvas");
+
+for (let i = 1; i <= 100; i++ ) {
+    const box = document.createElement("div");
+    box.setAttribute("style", "border: 1px solid black");
+    box.setAttribute("class","cell");
+    main.appendChild(box);
+}
+
+const color = document.querySelectorAll(".color");
+
+
+for (let elem of color) {
+    elem.addEventListener("click", event => {
+        const colorChange = elem.style["background-color"];
+        const current = document.querySelector("#current-color");
+        current.setAttribute("style", `background-color: ${colorChange}`);
+    })
+
+};
+
+ const cells = document.querySelectorAll(".cell");
+
+ for (let elem of cells) {
+    elem.addEventListener("click", event => {
+        const current2 = document.querySelector("#current-color").style["background-color"];
+        elem.setAttribute("style", `background-color: ${current2}`);
+    })
+ }
+
+
+
+
+
