178b423ecd799aa47c0b88173d44301a0e1a648e
ayoublos <abelibel@pursuit.org>
2024-03-16 16:25:47 -0700

    finished events lab

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -1,6 +1,18 @@
 let colors=document.querySelectorAll(`.color`)
 let currentColor=document.querySelector(`#current-color`)
 // console.log(colors)
+
+let main=document.querySelector(`#canvas`)
+
+for(let i=0;i<100;i++){
+    
+    let cell=document.createElement(`div`)
+    // cell.className(`.cell`)
+
+    main.append(cell)
+}
+
+
 for(let color of colors){
     // console.log(color)
 color.addEventListener(`click`,(event)=>{
