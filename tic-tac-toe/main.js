fad321546b20c31b9dd393bc2fa76749ead3785f
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-03-16 14:33:21 -0400

    functionality added to the tic tac toe, now its able to create x and o on different turns

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,9 +1,15 @@
 const container = document.querySelector(".tic-tac-toe");
 
+ const resetButton = document.querySelector("button")
+
+resetButton.addEventListener("click", (event) => {
+    event.target.container.reset();
+})
+
 const makeBoard = () => {
     for(let i = 0; i < 9; i++){
         const cell = document.createElement("div");
-        cell.className = "square";
+        cell.classList.add("square" , "empty") ;
         container.append(cell);
     }
 }
@@ -12,6 +18,21 @@ window.onload = makeBoard();
 
 const emptyCell = document.querySelectorAll(".square");
 
-emptyCell.forEach(ele => ele.addEventListener("click", (event) => {
-    event.target.innerText = "x";
-}))
+let acc = 0;
+
+const makeMove = () => {
+    emptyCell.forEach(ele => ele.addEventListener("click", (event) => {
+        acc++;
+        if(event.target.classList.contains("empty") && acc % 2 !== 0) {
+            event.target.innerText = "X";
+            event.target.classList.remove("empty");
+        } else if (event.target.classList.contains("empty") && acc % 2 === 0){
+            event.target.innerText = "O";
+            event.target.classList.remove("empty");
+        }
+    }))
+}
+
+makeMove()
+
+
